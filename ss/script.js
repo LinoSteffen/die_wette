11b83aa9b6d2e6ece1ab2f08edec412c56c286b3
Lino Steffen <linosteffen@gmx.ch>
2023-03-29 22:39:27 +0200

    dark_light mode v2

--- ss/script.js
+++ ss/script.js
@@ -10,16 +10,45 @@ function showImage(imageSrc) {
 }
 
 
-
+/*
 function myFunction() {
   var element = document.body;
   element.classList.toggle("dark-mode");
+  document.getElementById("Logo").classList.toggle("invertlogo")
 	if (element.classList.contains("dark-mode")) {
-    document.getElementById("Logo").src = "img/LS white.png";
     document.getElementById("switch").src = "img/moon.png";
     } else {
-    document.getElementById("Logo").src = "img/LS black.png";
     document.getElementById("switch").src = "img/sun.png";
     }
 }
 
+localStorage.setItem('ichbineinhuan', 'ja');
+const value = localStorage.getIttem("ichbineinhuan");
+localStorage.removeItem("Ichbineinhuan");
+*/
+
+
+function myFunction() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+  document.getElementById("Logo").classList.toggle("invertlogo")
+	if (element.classList.contains("dark-mode")) {
+    document.getElementById("switch").src = "img/moon.png";
+    localStorage.setItem("darkModeEnabled", "true"); // speichere den aktuellen Modus im Local Storage
+  } else {
+    document.getElementById("switch").src = "img/sun.png";
+    localStorage.setItem("darkModeEnabled", "false"); // speichere den aktuellen Modus im Local Storage
+  }
+}
+
+// überprüfe beim Laden der Seite, ob der Dark Mode aktiviert ist
+function restoreDarkMode() {
+  var isDarkModeEnabled = localStorage.getItem("darkModeEnabled"); // get the state from local storage
+  if (isDarkModeEnabled === "true") { // check if the state is true
+    document.body.classList.add("dark-mode"); // apply the class to the body
+    document.getElementById("switch").src = "img/moon.png";
+    document.getElementById("Logo").classList.toggle("invertlogo")
+  }
+}
+
+restoreDarkMode(); // rufe die Funktion beim Laden der Seite auf, um den Dark Mode zu überprüfen
